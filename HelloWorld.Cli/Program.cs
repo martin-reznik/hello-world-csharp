bad6f229aea1bd57808b3204c04ed81c2b8f58d8
Martin Reznik <marez@microsoft.com>
2021-11-05 15:29:06 +0100

    adding code

---
+++ HelloWorld.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld.Cli
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
